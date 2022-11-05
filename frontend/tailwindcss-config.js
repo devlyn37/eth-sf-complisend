32ff996e9372446da6b521627d118048f6d6ae2e
x00F <poofyiq@gmail.com>
2022-11-04 20:38:10 -0700

    base style

---
+++ frontend/tailwindcss-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+	  extend: {
+		colors: {
+		  
+		}
+	  },
+	},
+	variants: {},
+	plugins: [],
+	content: ["./client/*.{html,js,jsx,css}","./client/**/*.{html,js,jsx,css}","./public/index.html"]
+}
