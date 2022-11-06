85c5f71bc51e25539cff1bbb12640c6fc99e02e0
kourin <kourin.code@gmail.com>
2022-11-06 00:24:56 -0700

    Fix contract small issue

--- contracts/WrappedToken.sol
+++ contracts/WrappedToken.sol
@@ -225,7 +225,12 @@ contract WrappedToken is
         bytes memory data
     ) internal override {
         // minting or burning are pushed in other functions
-        if (from == address(0x0) || to == address(0x0)) {
+        if (
+            from == address(0x0) ||
+            to == address(0x0) ||
+            address(_push) == address(0x0) ||
+            _channelAddress == address(0x0)
+        ) {
             return;
         }
 
